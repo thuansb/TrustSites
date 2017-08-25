fcdcba7d27154db51b835a32b775cfb7859c4d85
DESKTOP-20PI695\thuan <thuan.suba@gmail.com>
2017-08-26 04:37:58 +0700

    add prod webpack config

--- prod.webpack.config.js
+++ prod.webpack.config.js
@@ -1,6 +1,5 @@
 var path = require('path');
 var webpack = require('webpack');
-var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: ['./src/app.js'],
@@ -28,7 +27,12 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.IgnorePlugin(/[^/]+\/[\S]+.prod$/),
-    new BundleAnalyzerPlugin()
+    new webpack.IgnorePlugin(/[^/]+\/[\S]+.dev$/),
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false,
+      compressor: {
+        warnings: false
+      }
+    })
   ]
 }
